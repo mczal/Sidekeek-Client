4aa03bf80a326ecda359be046dec3ee1416bae2a
Adli Fariz Bonaputra <abathz@gmail.com>
2016-03-12 01:42:09 +0700

    update company profile

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -271,5 +271,51 @@ appController.controller('EditProfileController', ['$scope', '$http',
                 console.log(error);
             }
         });
+
+        $scope.addPorto = function (){
+            $(".overlay").show();
+            $(".offcanvas-portofolio").show();
+        }
+
+        var produk = [
+        '<div class="edit-profile-full">',
+        '                        <div class="row" ng-repeat="dataProduct in dataProducts">',
+        '                            <div class="col-lg-4">',
+        '                                <div class="upload text-center">',
+        '                                    <div class="box container-fluid">',
+        '                                        <div class="file-upload text-center">',
+        '                                            <img src="assets/img/uploadfoto.png" alt="" />',
+        '                                            <input type="file" name="file" id="file-product-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple />',
+        '                                            <label for="file-product-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/></svg><span>Choose a file&hellip;</span></label>',
+        '                                        </div>',
+        '                                    </div>',
+        '                                    <div class="box-kecil">',
+        '                                        <img src="assets/img/uploadfoto.png" alt="" />',
+        '                                    </div>',
+        '                                    <div class="box-kecil">',
+        '                                        <img src="assets/img/uploadfoto.png" alt="" />',
+        '                                    </div>',
+        '                                    <div class="box-kecil">',
+        '                                        <img src="assets/img/uploadfoto.png" alt="" />',
+        '                                    </div>',
+        '                                </div>',
+        '                            </div>',
+        '                            <div class="col-lg-8">',
+        '                                <div class="form-group form-group-xl">',
+        '                                    <input type="text" class="form-control" placeholder="Nama Produk" value="" ng-model="form.namaProduk">',
+        '                                </div>',
+        '                                <div class="form-group form-group-xl">',
+        '                                    <input type="text" class="form-control" placeholder="Harga" value="" ng-model="form.harga">',
+        '                                </div>',
+        '                                <div class="form-group">',
+        '                                    <textarea type="text" class="form-control" rows="10" placeholder="Product Description" ng-model="form.descProduk"></textarea>',
+        '                                </div>',
+        '                            </div>',
+        '                        </div>',
+        '                    </div>'
+        ].join('');
+        $scope.addProduk = function (){
+            $(".add-produk").append(produk);
+        }
     }
 ]);
