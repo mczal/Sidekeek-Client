3332df98e11fe97eb5c027bd3b9b36ca764f352a
Kenan Christian <christian.dimas.94@gmail.com>
2017-03-04 18:29:02 +0700

    clean up confirmation logic

--- src/assets/js/controllers/confirmationController.js
+++ src/assets/js/controllers/confirmationController.js
@@ -2,46 +2,31 @@ angular.module('app.confirmation',[])
 
 .controller('ConfirmationController',confirmationController);
 
-confirmationController.$inject = ['$scope', '$http', '$timeout', '$window', '$location','$stateParams','userService','authService'];
+confirmationController.$inject = ['$scope', '$http', '$state', '$stateParams','userService','authService'];
 
-function confirmationController($scope, $http, $timeout, $window, $location,$stateParams,userService,authService){
-  var unique = $stateParams.uq;
-  userService.confirmAccount(unique).success(function(data){
-    console.log(data);
-    if (data.error != undefined){
+function confirmationController($scope, $http, $state, $stateParams, userService, authService){
+  authService.getToken().success(function(data) {
+    authService.setToken(data.token);
 
-    }
+    userService.confirmAccount($stateParams.uq).success(function(data){
+      if (data.error != undefined){
+        console.log(data);
+        debugger;
+      }
+
       localStorage.setItem('session', data.session);
       localStorage.setItem('emailHost', data.email);
       localStorage.setItem('idHost', data.idHost);
       authService.setToken(data.token);
-      var redirectTimeout;
+
       if (localStorage.getItem('tipeMember') == 0) {
-          var redirect = function(){
-              $window.location.href = '#/home'
-              $window.location.reload();
-          }
-      }else{
-          var redirect = function(){
-              $window.location.href = '#/account-host'
-              $window.location.reload();
-          }
+        $state.go("home");
+      } else {
+        $state.go("account-host");
       }
-      // var redirect = function() {
-      //     if (localStorage.getItem('tipeMember') == 0) {
-      //         $window.location.href = '#/home'
-      //         $window.location.reload();
-      //     }else{
-      //         $window.location.href = '#/account-temp'
-      //         $window.location.reload();
-      //     }
-      // }
-      $timeout.cancel(redirectTimeout);
-      redirectTimeout = $timeout(function() {
-          var timeoutTime = 5000;
-          redirectTimeout = $timeout(redirect, timeoutTime);
-      });
-  }).error(function(data){
-      console.log(data.message);
-  });
+
+    }).error(function(data){
+        console.log(data.message);
+    });
+  })
 };
